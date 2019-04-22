1a9de191c6895840f189edb946180c13cf67f81a
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-22 16:27:46 +0200

    Remove CssBaseline component

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react'
 import EventListItem from './EventListItem';
-import CssBaseline from '@material-ui/core/CssBaseline';
+
 class EventList extends Component {
   render() {
     return (
       <div>
-        <CssBaseline />
         <EventListItem />
       </div>
     )
