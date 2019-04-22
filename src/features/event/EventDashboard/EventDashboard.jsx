38f08901eac5ccb0386542b0b805d5dfa66576f7
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-22 15:21:51 +0200

    Adjust grid to center EventList inside the dashboard

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+
+/* Material UI Components */
 import Grid from '@material-ui/core/Grid';
+
+/* Components */
+import EventList from '../EventList/EventList';
 
 class EventDashboard extends Component {
   render() {
     return (
       <Grid container spacing={24}>
-        <Grid item sm={8} xs={12}>
-          Left Pane
+        <Grid container justify="center" align="center" item sm={8} xs={12}>
+          <EventList />
       </Grid>
         <Grid item sm={4} xs={12}>
           Right Pane
