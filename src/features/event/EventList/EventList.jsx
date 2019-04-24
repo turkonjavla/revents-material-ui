92f38341cdbd09269826294acc48993b0e0f7628
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-24 18:11:21 +0200

    Update EventList component

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import EventListItem from './EventListItem';
 
 class EventList extends Component {
   render() {
+    const { events } = this.props;
+    const eventList = events.map(event => <EventListItem key={event.id} event={event} />)
     return (
-      <div>
-        <EventListItem />
-      </div>
+      <Fragment>
+        {eventList}
+      </Fragment>
     )
   }
 }
