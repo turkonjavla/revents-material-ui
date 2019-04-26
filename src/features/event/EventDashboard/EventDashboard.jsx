8889117c7142ea9dbe6cda421de0dc3719f3ec1d
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-26 12:40:19 +0200

    Fetch events from redux

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,4 +1,6 @@
 import React, { Component, Fragment } from 'react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
 import cuid from 'cuid';
 
 /* Material UI Components */
@@ -10,57 +12,6 @@ import Button from '@material-ui/core/Button';
 /* Components */
 import EventList from '../EventList/EventList';
 import EventForm from '../EventForm/EventForm';
-
-const events = [
-  {
-    id: '1',
-    title: 'Trip to Tower of London',
-    date: '2018-03-27',
-    category: 'culture',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: "Tower of London, St Katharine's & Wapping, London",
-    hostedBy: 'Bob',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    attendees: [
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      },
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      }
-    ]
-  },
-  {
-    id: '2',
-    title: 'Trip to Punch and Judy Pub',
-    date: '2018-03-28',
-    category: 'drinks',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sollicitudin ligula eu leo tincidunt, quis scelerisque magna dapibus. Sed eget ipsum vel arcu vehicula ullamcorper.',
-    city: 'London, UK',
-    venue: 'Punch & Judy, Henrietta Street, London, UK',
-    hostedBy: 'Tom',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
-    attendees: [
-      {
-        id: 'b',
-        name: 'Tom',
-        photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-      },
-      {
-        id: 'a',
-        name: 'Bob',
-        photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-      }
-    ]
-  }
-];
 
 const styles = theme => ({
   grid: {
@@ -82,7 +33,6 @@ const styles = theme => ({
 
 class EventDashboard extends Component {
   state = {
-    events: events,
     isOpen: false,
     selectedEvent: null
   }
@@ -146,8 +96,8 @@ class EventDashboard extends Component {
   }
 
   render() {
-    const { classes } = this.props;
-    const { events, isOpen, selectedEvent } = this.state;
+    const { classes, events } = this.props;
+    const { isOpen, selectedEvent } = this.state;
     return (
       <Fragment>
         <div className={classes.root}>
@@ -196,4 +146,11 @@ class EventDashboard extends Component {
   }
 }
 
-export default withStyles(styles)(EventDashboard);
+const mapStateToProps = state => ({
+  events: state.events
+})
+
+export default compose(
+  connect(mapStateToProps),
+  withStyles(styles)
+)(EventDashboard);
