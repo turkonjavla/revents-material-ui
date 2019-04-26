859948a104fe96e1779451c6c240778f0c3c344d
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-04-26 12:07:02 +0200

    Test a simple increment/decrement counter with redux

--- src/features/testarea/TestComponent.jsx
+++ src/features/testarea/TestComponent.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
 import { compose } from 'redux';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
+
+/* Test Actions */
+import { incrementCounter, decrementCounter } from './testActions';
 
 class TestComponent extends Component {
   render() {
-    const { data } = this.props;
+    const { data, incrementCounter, decrementCounter } = this.props;
     return (
       <div style={{padding: '4em'}}>
         <h1>Test Area</h1>
         <h2>{data}</h2>
+        <Button onClick={incrementCounter} variant="outlined" color="primary">Increment</Button>
+        <Button onClick={decrementCounter} variant="outlined" color="secondary">Decrement</Button>
       </div>
     )
   }
@@ -18,6 +24,11 @@ const mapStateToProps = state => ({
   data: state.test.data
 })
 
+const actions = {
+  incrementCounter,
+  decrementCounter
+}
+
 export default compose(
-  connect(mapStateToProps)
+  connect(mapStateToProps, actions)
 )(TestComponent);
