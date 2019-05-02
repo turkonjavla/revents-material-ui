37fb243ba2d33ad88d4f792bd1d2b725973c05c4
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-05-02 12:36:09 +0200

    Add modalReducer to rootReducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -4,11 +4,13 @@ import { reducer as formReducer } from 'redux-form';
 /* Custom Reducers */
 import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
+import modalReducer from '../../features/modals/modalReducer';
 
 const rootReducer = combineReducers({
   test: testReducer,
   events: eventReducer,
-  form: formReducer
+  form: formReducer,
+  modals: modalReducer
 })
 
 export default rootReducer;
