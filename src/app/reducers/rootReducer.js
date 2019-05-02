0e83893b23e066ecfafae0d62a829aa520ac03e8
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-05-02 19:19:25 +0200

    Add async constants, actions and reducer

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -6,13 +6,15 @@ import testReducer from '../../features/testarea/testReducer';
 import eventReducer from '../../features/event/eventReducer';
 import modalReducer from '../../features/modals/modalReducer';
 import authReducer from '../../features/auth/authReducer';
+import asyncReducer from '../../features/async/asyncReducer';
 
 const rootReducer = combineReducers({
   test: testReducer,
   events: eventReducer,
   form: formReducer,
   modals: modalReducer,
-  auth: authReducer
+  auth: authReducer,
+  async: asyncReducer
 })
 
 export default rootReducer;
