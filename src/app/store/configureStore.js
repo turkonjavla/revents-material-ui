4eb6b18aeef0a6bfd817459eab499e738ba60567
Vladimir Turkonja <turkonjavlada@gmail.com>
2019-05-03 15:35:25 +0200

    Configure store for firebase and firestore

--- src/app/store/configureStore.js
+++ src/app/store/configureStore.js
@@ -1,15 +1,29 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+
+import firebase from '../config/firebase';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer';
 
+const rrfConfig = {
+  userProfile: 'users',
+  attachAuthIsReady: true,
+  useFirestoreForProfile: true
+}
+
 export const configureStore = preloadedState => {
-  const middlewares = [thunk];
+  const middlewares = [thunk.withExtraArgument({ getFirebase, getFirestore })];
   const middlewareEnchancer = applyMiddleware(...middlewares);
 
   const storeEnchancers = [middlewareEnchancer];
 
-  const composedEnchancer = composeWithDevTools(...storeEnchancers);
+  const composedEnchancer = composeWithDevTools(
+    ...storeEnchancers,
+    reactReduxFirebase(firebase, rrfConfig),
+    reduxFirestore(firebase)
+  );
 
   const store = createStore(
     rootReducer,
